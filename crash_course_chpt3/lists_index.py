6c39390aa6d1377f501779840dad762471667864
Calvin Coxen <calvincoxen@selinc.com>
2024-05-31 12:26:00 -0700

    Initial Commit

---
+++ crash_course_chpt3/lists_index.py
@@ -0,0 +1,18 @@
+
+#begin
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+#useful for accessing last item in lists w/unknown length
+print(bicycles[-1].title())
+
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
+
+
+
+#end
