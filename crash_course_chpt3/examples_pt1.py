d099bb0357f9b54a7136b5b6f15f506de35a7cc6
Calvin Coxen <calvincoxen@selinc.com>
2024-05-31 12:44:37 -0700

    Initial Commit

---
+++ crash_course_chpt3/examples_pt1.py
@@ -0,0 +1,27 @@
+
+#begin
+#3-1 Names
+names = ['Dylan Neb', 'Sean Geen','Tyso Chicken','Mr. Scack']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+#3-2 Greetings
+
+print (f"Hello {names[0]}, sussy baka")
+print (f"Hello {names[1]}, sussy baka")
+print (f"Hello {names[2]}, sussy baka")
+print (f"Hello {names[3]}, sussy baka")
+
+#3-3 Your Own List
+transportation = ['plane', 'boat', 'car', 'elon musk high speed underground tunnel system']
+
+print(f"I like {transportation[0].title()}")
+print(f"{transportation[1].title()} is cool")
+print(f"I go away by {transportation[2].title()}")
+print(f"everyone hates {transportation[3].upper()}")
+
+
+#end
