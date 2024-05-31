3786dfe110e95916d28197a6fe30624b6c40cd5a
Calvin Coxen <calvincoxen@selinc.com>
2024-05-31 12:17:02 -0700

    Initial Commit

---
+++ crash_course_chpt2/try_it_yourself_pt1.py
@@ -0,0 +1,30 @@
+
+
+#begin
+#2-3 Personal Message
+name = "Eric"
+print(f"Hello {name}, would you like to learn some python today?")
+
+#2-4 Name Cases:
+full_name = "daBId robOTnix"
+print(full_name.lower())
+print(full_name.upper())
+print(full_name.title())
+
+#2-5 Famous Quote
+print('Marcus Arelius once said, \n"Our life is what our thoughts make it."')
+
+#2-6 Famous Quote 2
+famous_person = "Marcus Arelius"
+quote = '"Our life is what our thoughts make it."'
+message = f"\t{famous_person} once said, \n\t{quote}"
+print(message)
+
+#2-7 Stripping Names
+test_name = " dabid "
+print(test_name)
+print(test_name.lstrip())
+print(test_name.rstrip())
+print(test_name.strip())
+
+#end
