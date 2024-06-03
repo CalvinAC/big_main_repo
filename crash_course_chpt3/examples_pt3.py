b26252ed980b8e81237e3430daf4afed40882e42
Calvin Coxen <calvincoxen@selinc.com>
2024-06-03 15:36:15 -0700

    Initial Commit

---
+++ crash_course_chpt3/examples_pt3.py
@@ -0,0 +1,23 @@
+#CalvinAC
+#6/3/24
+#examples_pt3.py
+
+#3-8: Seeing the world
+
+locations = ['ireland', 'china', 'gaza', 'uganda', 'earth']
+print(locations)
+print(sorted(locations))
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.sort()
+print(locations)
+
+locations.reverse()
+print(locations)
+
