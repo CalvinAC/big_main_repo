918b9f53127fdb2fcf9538c589e3847eda1397b7
Calvin Coxen <calvincoxen@selinc.com>
2024-06-04 16:55:21 -0700

    Initial Commit

---
+++ crash_course_chpt4/list_loops.py
@@ -0,0 +1,15 @@
+#CalvinAC
+#6/4/24
+#list_loops.py
+
+#==============================================================================
+
+#for loops
+magicians = ['alice', 'david','carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magix show!")
+
+#end
