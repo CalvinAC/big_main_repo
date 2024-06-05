fdc1be81466f284253055cf57bac0ba6b974e669
Calvin Coxen <calvincoxen@selinc.com>
2024-06-04 17:03:45 -0700

    Initial Commit

---
+++ crash_course_chpt4/examples_pt1.py
@@ -0,0 +1,23 @@
+#CalvinAC
+#6/4/24
+#examples_pt1.py
+
+#==============================================================================
+
+#4-1 Pizzas
+
+pizzas = ['cheese', 'pepperoni', 'supreme']
+for pizza in pizzas:
+    print(f"I like {pizza} pizzas")
+
+print("I require pizza for sustenance")
+
+#4-2 Animals 
+
+animals = ['bat', 'bird', 'lizard']
+for animal in animals:
+    print(f"A {animal} would make a great pet")
+
+print("Heck, they all make great pets!")
+
+#end
