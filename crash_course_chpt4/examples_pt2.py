46cdb4a0445c433b054b48e176464ab1458d2084
Calvin Coxen <calvincoxen@selinc.com>
2024-06-06 15:15:04 -0700

    Initial Commit

---
+++ crash_course_chpt4/examples_pt2.py
@@ -0,0 +1,32 @@
+#CalvinAC
+#6/6/24
+#examples_pt2
+# 
+#==============================================================================
+
+#4-3 Counting to Twenty
+#==============================================================================
+value = 0
+for value in range(1,21):
+    print(value)
+    value += 1
+
+#4-4 One Million
+#==============================================================================
+num = [0]
+for nums in range(1,1_000_001):
+    num.append(nums + 1)
+    #print(nums)    #commented out because it takes forever to run this
+
+#4-5 Summing a Million
+#==============================================================================
+num = [0]
+for nums in range(1,1_000_001):
+    num.append(nums + 1)
+    
+# print(min(nums))
+# print(max(nums))
+# print(sum(nums))
+
+#4-6 
+
