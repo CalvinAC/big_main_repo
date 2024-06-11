d83c5519ecbb6e1e817a3286ca62be9cb0e044de
Calvin Coxen <calvincoxen@selinc.com>
2024-06-11 13:49:58 -0700

    Finished

--- crash_course_chpt4/list_tuples.py
+++ crash_course_chpt4/list_tuples.py
@@ -5,10 +5,15 @@
 #==============================================================================
 
 #tuple looks like a list but is unmutable
+#works for things that should not be changed
 dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
 
 for dimension in dimensions:
     print(dimension)
-    
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
