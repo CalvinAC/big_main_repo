958c507974ad9a269d6b57a4554cb9533272ab1f
Calvin Coxen <calvincoxen@selinc.com>
2024-06-11 13:34:52 -0700

    Initial Commit

---
+++ crash_course_chpt4/examples_pt3.py
@@ -0,0 +1,23 @@
+#CalvinAC
+#6/11/24
+#examples_pt3.py
+# 
+#==============================================================================
+
+#4-10 Slices
+#==============================================================================
+
+rand_list = ['spaghetti', 'mario', 'dunk', 'trogdor', 'chungus']
+
+print("The first three items in the list are:")
+print(rand_list[:3])
+
+print("Three items in the middle of the list are:")
+print(rand_list[1:4])
+
+print("The last three items in the list are:")
+print(rand_list[2:])
+
+#4-11 My Pizzas, Your Pizzas
+#==============================================================================
+
