c28b970f4ebfdd3f38a7d3a61d6bdca3adea175e
Calvin Coxen <calvincoxen@selinc.com>
2024-06-11 13:30:05 -0700

    Finished section

--- crash_course_chpt4/list_parts.py
+++ crash_course_chpt4/list_parts.py
@@ -15,9 +15,15 @@ print("Here are the first three players on my team:")
 for player in players [:3]:
     print(player.title())
 
-# my_foods = ['pizza', 'falafel', 'carrot cake']
-# friend_foods = my_foods[:]
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
 
-# print("My favourite foods are:")
-# print(my_foods())
+#proves that lists are different
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
 
+print("My favourite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
