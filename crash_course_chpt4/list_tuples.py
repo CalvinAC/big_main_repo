e3ffa6e24b7dd5698cb9caabfd1f6057a8221119
Calvin Coxen <calvincoxen@selinc.com>
2024-06-11 13:47:07 -0700

    Initial comm

---
+++ crash_course_chpt4/list_tuples.py
@@ -0,0 +1,14 @@
+#CalvinAC
+#6/11/24
+#list_tuples.py
+# 
+#==============================================================================
+
+#tuple looks like a list but is unmutable
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+    print(dimension)
+    
