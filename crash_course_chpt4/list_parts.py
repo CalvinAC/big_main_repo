8f117a284ff3d7fd9b92ef0435da37b152973f02
Calvin Coxen <calvincoxen@selinc.com>
2024-06-11 13:25:52 -0700

    Initial Commit

---
+++ crash_course_chpt4/list_parts.py
@@ -0,0 +1,23 @@
+#CalvinAC
+#6/11/24
+#list_parts.py
+# 
+#==============================================================================
+
+players = ['charles', 'martina', 'michael', 'florence']
+print(players[0:3])
+
+#defaults to beginning of list
+print(players[:4])
+
+#slicing loop
+print("Here are the first three players on my team:")
+for player in players [:3]:
+    print(player.title())
+
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+
+# print("My favourite foods are:")
+# print(my_foods())
+
