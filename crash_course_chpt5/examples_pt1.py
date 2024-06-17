cb9d8b659ce779bbc8b407d45b1afca6c5fabe2f
Calvin Coxen <calvincoxen@selinc.com>
2024-06-17 15:03:55 -0700

    Initial Commit

---
+++ crash_course_chpt5/examples_pt1.py
@@ -0,0 +1,18 @@
+#CalvinAC
+#6/17/24
+#examples_pt1.py
+# 
+#==============================================================================
+
+#5-1 Conditional Test
+#==============================================================================
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False")
+print(car == 'audi')
+
+#5-2 More Conditional Tests
+#==============================================================================
