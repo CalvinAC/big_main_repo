4b50077617eefe64900d708c856b7ccbdc8c2e01
Calvin Coxen <calvincoxen@selinc.com>
2024-06-17 15:28:34 -0700

    Initial Commit

---
+++ crash_course_chpt5/examples_pt2.py
@@ -0,0 +1,16 @@
+#CalvinAC
+#6/17/24
+#examples_pt2.py
+# 
+#==============================================================================
+
+# 5-8 Hello Admin
+#==============================================================================
+
+user_names = ['admin', 'david', 'joe', 'sam', 'bill']
+
+for username in user_names:
+    if 'admin 'in user_names == True:
+        print('Hello Mr. Bond')
+    else:
+        print(f"Welcome back {user_names}")
