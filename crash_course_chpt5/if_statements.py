cbbcc3fc1b2d8a1a4de642ff8ed047a44ac4d036
Calvin Coxen <calvincoxen@selinc.com>
2024-06-17 15:16:14 -0700

    More stuff

--- crash_course_chpt5/if_statements.py
+++ crash_course_chpt5/if_statements.py
@@ -4,3 +4,16 @@
 # 
 #==============================================================================
 
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            print("Sorry, we are out of green peppers rn RIP")
+        else:
+            print(f"Adding {requested_topping}.")
+
+    print("Finished making da pizza pie")
+else:
+    print('You sure you want just bread?')
