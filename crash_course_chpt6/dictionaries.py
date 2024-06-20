59ea259810088d5b4ff79b74af0581c17f4a47b5
Calvin Coxen <calvincoxen@selinc.com>
2024-06-20 12:24:03 -0700

    Initial Commit

---
+++ crash_course_chpt6/dictionaries.py
@@ -0,0 +1,10 @@
+#CalvinAC
+#6/20/24
+#dictionaries.py
+# 
+#==============================================================================
+
+alien_0 = {'color': 'green', 'points': 5 }
+
+print(alien_0['color'])
+print(alien_0['points'])
