6735a1398c793a360aa553d46462ab3e657583fe
Calvin Coxen <calvincoxen@selinc.com>
2024-06-20 13:30:16 -0700

    leedle

--- crash_course_chpt6/loop_dictionaries.py
+++ crash_course_chpt6/loop_dictionaries.py
@@ -25,6 +25,14 @@ favorite_languages = {
 for name, language in favorite_languages.items():
     print(f"{name.title()}'s favorite language is {language.title()}")
 
+friends = ['phil', 'sarah']
 #loops through just the keys in the dictionary
+#This also works: for name in favorite_languages:
 for name in favorite_languages.keys():
-    print(name.title())
+    print(f"Hi {name.title()}.")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+
