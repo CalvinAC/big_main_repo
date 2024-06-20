8a2b02633c12032d71bb4a6e58d95297860a8996
Calvin Coxen <calvincoxen@selinc.com>
2024-06-20 12:55:01 -0700

    fav languages

--- crash_course_chpt6/dictionaries.py
+++ crash_course_chpt6/dictionaries.py
@@ -17,3 +17,37 @@ alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
 
+#delete function
+del alien_0['points']
+print(alien_0)
+
+#alien_0 = {} creates an empty dictionary
+#add items to dictionary by following lines 16 and 17
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+
+#Move the alien to the right.
+#Determine how far to move the alien based on its current speed
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+#The new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}")
+
+#end
