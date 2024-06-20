9ca00299b767979b4bca1ee45ed5b5dc78842f98
Calvin Coxen <calvincoxen@selinc.com>
2024-06-20 13:10:23 -0700

    Inital Commit

---
+++ crash_course_chpt6/loop_dictionaries.py
@@ -0,0 +1,15 @@
+#CalvinAC
+#6/20/24
+#loop_dictionaries.py
+# 
+#==============================================================================
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi'
+}
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"\nValue: {value}")
