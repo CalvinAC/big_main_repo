52dff69084b44d47832a429078e35d92b9d36451
Calvin Coxen <calvincoxen@selinc.com>
2024-07-05 16:16:15 -0700

    This sounds like robot pirate island!!!

--- crash_course_chpt7/input_function.py
+++ crash_course_chpt7/input_function.py
@@ -6,3 +6,16 @@
 
 message = input("Tell me something and I will repeat it back to you: ")
 print(message)
+
+name= input("Please enter your name:")
+print(f"\nHello, {name}")
+
+prompt = ("If you tell us who youe are we can personalize the messages you see.")
+prompt += "\nWhat is your first name?"
+
+name = input(prompt)
+print(f"\nHello, {name}")
+
+age = input('How old are you?')
+age = int(age)
+print(age >= 18)
