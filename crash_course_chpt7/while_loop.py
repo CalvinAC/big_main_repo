aa620626f1bb1cae4b01a08e4be8b4665e5521de
Calvin Coxen <calvincoxen@selinc.com>
2024-07-05 16:31:42 -0700

    thems the breaks

--- crash_course_chpt7/while_loop.py
+++ crash_course_chpt7/while_loop.py
@@ -29,5 +29,14 @@ while active:
     else:
         print(message)
 
+#breaks
 prompt = "\nPlease enter the name of a city you have visited: "
 prompt += "\nEnter 'quit' to end the program. "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
