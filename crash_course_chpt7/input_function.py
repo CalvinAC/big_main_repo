53b57ac561171df5254c67c90762466cad509ab6
Calvin Coxen <calvincoxen@selinc.com>
2024-07-05 16:07:36 -0700

    Initial Commit

---
+++ crash_course_chpt7/input_function.py
@@ -0,0 +1,8 @@
+#CalvinAC
+#7/5/24
+#input_function.py
+# 
+#==============================================================================
+
+message = input("Tell me something and I will repeat it back to you: ")
+print(message)
