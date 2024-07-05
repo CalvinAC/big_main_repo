dfbec9b266383a48d87fd03a990cd6546ec122ab
Calvin Coxen <calvincoxen@selinc.com>
2024-07-05 10:43:04 -0700

    6-7

--- crash_course_chpt6/examples_pt2.py
+++ crash_course_chpt6/examples_pt2.py
@@ -6,3 +6,19 @@
 
 # 6-7 People
 #==============================================================================
+person_1 = {'first name': 'captain',
+           'last name': 'falcon',
+           'age': '33',
+           'city': 'F zero'
+           }
+
+person_2 = {'first name': 'jimmy',
+           'last name': 'neutron',
+           'age': '8',
+           'city': 'retroville'
+           }
+
+peoples = [person_1, person_2]
+
+for people in peoples:
+    print(people)
