6f06350ce36a67318dd5adb41b84d20086ffdfbb
Calvin Coxen <calvincoxen@selinc.com>
2024-07-15 16:02:56 -0700

    ADded stuff

--- crash_course_chpt7/loops_n_stuff.py
+++ crash_course_chpt7/loops_n_stuff.py
@@ -4,8 +4,8 @@
 # 
 #==============================================================================
 
-#star with users that need to be verified,
-#and an empty list to hold confirmed users.
+#Start with users that need to be verified,
+# and an empty list to hold confirmed users.
 
 unconfirmed_users = ['alice', 'brian', 'candace']
 confirmed_users = []
@@ -22,3 +22,37 @@ while unconfirmed_users:
 print("\nThe following users have been confirmed:")
 for confirmed_user in confirmed_users:
     print(confirmed_user.title())
+
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+#while loop uses remove function to remove every instance of cats in list
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
+
+responses = {}
+
+# SEt a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name?")
+    response = input("Which mountain would you like to climb someday?")
+
+    #Store the responses in the dictionary
+    responses[name] = response
+
+    #Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/no)")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Show the results.
+print('\n--- Poll Results ---')
+for name, response in responses.items():
+    print(f"{name} would like to climb {response}.")
+
