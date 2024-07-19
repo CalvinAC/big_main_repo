5aed93e5a20670b64292235706a76ce3332d3033
Calvin Coxen <calvincoxen@selinc.com>
2024-07-19 10:02:40 -0700

    Initial Commit

---
+++ crash_course_chpt8/functions.py
@@ -0,0 +1,11 @@
+#CalvinAC
+#7/19/24
+#functions.py
+# 
+#==============================================================================
+
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+greet_user()
