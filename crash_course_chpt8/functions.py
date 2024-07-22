7cd2dedf949632ed7502cdca6e7fa7682e1e2a23
Calvin Coxen <calvincoxen@selinc.com>
2024-07-22 12:36:27 -0700

    again

--- crash_course_chpt8/functions.py
+++ crash_course_chpt8/functions.py
@@ -20,3 +20,27 @@ def describe_pet(animal_type, pet_name):
 
 describe_pet('hamster','harry')
 describe_pet('dog', 'willie')
+
+#Keyword arguements
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(pet_name='harry', animal_type='hamster')
+
+def describe_pet( pet_name, animal_type='dog'):
+    """Display informaton about a pet"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(pet_name='willie')
+describe_pet(pet_name='harry', animal_type='hamster')
+
+#A dog named Willie.
+describe_pet('willie')
+describe_pet(pet_name='willie')
+
+#A hamster named harry
+describe_pet('harry', 'hamster')
+describe_pet(pet_name='harry',animal_type='hamster')
+describe_pet(animal_type='hamster', pet_name='harry')
+
+#Will cause arguement error-
+#describe_pet()
