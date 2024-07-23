e3e467b627adefd950d4715929d7259b8ffda0b9
Calvin Coxen <calvincoxen@selinc.com>
2024-07-23 16:39:40 -0700

    Initial Commit

---
+++ crash_course_chpt8/returns.py
@@ -0,0 +1,13 @@
+#CalvinAC
+#7/23/24
+#returns.py
+# 
+#==============================================================================
+
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
