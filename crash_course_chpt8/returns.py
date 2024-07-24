33bc1b6819bf7cfb37f6c418bbf73fd662332348
Calvin Coxen <calvincoxen@selinc.com>
2024-07-24 09:47:19 -0700

    eep

--- crash_course_chpt8/returns.py
+++ crash_course_chpt8/returns.py
@@ -19,3 +19,20 @@ print(musician)
 musician = get_formatted_name('john', 'lee', 'hooker')
 print(musician)
 
+def build_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person"""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
+
+while True:
+    print('\nPlease tell me your name: ')
+    f_name = input("First name: ")
+    l_name = input('Last name: ')
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHellos, {formatted_name}!")
