3fe223b33831859c6ab7dce18cc65c5fd0a230e2
Calvin Coxen <calvincoxen@selinc.com>
2024-07-26 09:48:48 -0700

    Importer exporter

--- crash_course_chpt8/modules.py
+++ crash_course_chpt8/modules.py
@@ -10,4 +10,15 @@ pizza_pie.make_pizza(16, 'pepperoni')
 pizza_pie.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
 
+from pizza_pie import make_pizza
 
+make_pizza(17, 'sus sauce')
+
+
+from pizza_pie import make_pizza as mp
+
+mp(47, 'anchovies', 'bottom text')
+
+import pizza_pie as pi
+
+pi.make_pizza(55, 'bananas')
