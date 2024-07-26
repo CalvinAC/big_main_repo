01052b826c3223b3f3b0fa5a716a0c8385cecb8b
Calvin Coxen <calvincoxen@selinc.com>
2024-07-26 09:40:46 -0700

    Pretty epic!

--- crash_course_chpt8/modules.py
+++ crash_course_chpt8/modules.py
@@ -4,7 +4,10 @@
 # 
 #==============================================================================
 
+import pizza_pie
 
+pizza_pie.make_pizza(16, 'pepperoni')
+pizza_pie.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
 
 
