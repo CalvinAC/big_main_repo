2108414184f594679d2ee5853c78687ffc835605
Calvin Coxen <calvincoxen@selinc.com>
2024-08-05 16:34:26 -0700

    mmmmlem

--- crash_course_chpt9/Instances.py
+++ crash_course_chpt9/Instances.py
@@ -22,7 +22,21 @@ class Car:
     def read_odometer(self):
         """Print a statement showig the car's mileage"""
         print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it atempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
     
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reading = 23
+my_new_car.update_odometer(45)
 my_new_car.read_odometer()
