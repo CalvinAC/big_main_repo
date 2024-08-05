e7476b3231a286425656bab501fb1041de8eb1c7
Calvin Coxen <calvincoxen@selinc.com>
2024-08-05 16:53:24 -0700

    Finnito Burrito

--- crash_course_chpt9/Instances.py
+++ crash_course_chpt9/Instances.py
@@ -33,10 +33,16 @@ class Car:
         else:
             print("You can't roll back an odometer!")
 
+    def increment_odometer(self,miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
     
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
 
-my_new_car.odometer_reading = 23
-my_new_car.update_odometer(45)
+my_new_car.update_odometer(23_500)
+my_new_car.read_odometer()
+
+my_new_car.increment_odometer(100)
 my_new_car.read_odometer()
